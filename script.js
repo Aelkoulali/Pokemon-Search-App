8ab824ee341c9678dd28e399cc6a4d24be96715b
Aelkoulali <abdellah.elkoulali@gmx.fr>
2024-09-20 19:41:12 +0200

    Declare variables

---
+++ script.js
@@ -0,0 +1,15 @@
+// Declare variables
+const searchForm = document.getElementById("search-form");
+const searchInput = document.getElementById("search-input");
+const pokemonId = document.getElementById("pokemon-id");
+const pokemonName = document.getElementById("pokemon-name");
+const spriteCtn = document.getElementById("sprite-container");
+const types = document.getElementById("types");
+const weight = document.getElementById("weight");
+const height = document.getElementById("height");
+const hp = document.getElementById("hp");
+const attack = document.getElementById("attack");
+const defense = document.getElementById("defense");
+const spAttack = document.getElementById("special-attack");
+const spDefense = document.getElementById("special-defense");
+const speed = document.getElementById("speed");
