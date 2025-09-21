83e6e063dbe00a20c96f28c8117b40c5df37c41a
Abdellah <abdellah.elkoulali@gmx.fr>
2025-09-21 10:26:41 +0200

    Add colors based on type

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ searchBtn.addEventListener("click", (e) => {
         alert("Pokémon not found");
     } else {
         // Proceed with searching the Pokémon (you can add your fetch code here)
-        fetchPokemonData(searchInput.value);
+        fetchPokemonData(searchInput.value);        
     }
 });
 
@@ -42,7 +42,7 @@ async function fetchPokemonData(pokemon) {
         displayPokemonData(data);
     } catch (error) {
         console.error("Error fetching Pokémon data:", error);
-    }
+    }    
 }
 
 // Function to display Pokémon data
@@ -62,3 +62,25 @@ function displayPokemonData(data) {
 }
 // Initial fetch for a default Pokémon (e.g., Pikachu)
 fetchPokemonData("pikachu");
+
+// add colors based on type
+const typeColors = {
+   normal: '#A8A77A',
+   fire: '#FBA54C',
+   water: '#A4C8E1',
+   electric: '#F2D94F',
+   grass: '#A7C74C',
+   ice: '#A0D8E1',
+   fighting: '#C22E28',
+   poison: '#A33EA1',
+   ground: '#E2BF65',
+   flying: '#A98FF3',
+   psychic: '#F95587',
+   bug: '#A6B91A',
+   rock: '#B6A136',
+   ghost: '#735797',
+   dragon: '#6F35FC',
+   dark: '#705746',
+   steel: '#B7B7CE',
+   fairy: '#D685AD',
+};
